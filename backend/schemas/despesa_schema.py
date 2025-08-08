e85db1238f63ca9ee47c90204715d1f99f8c2a7e
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:57:35 -0300

    Update despesa_schema.py

--- backend/schemas/despesa_schema.py
+++ backend/schemas/despesa_schema.py
@@ -1,1 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class DespesaBase(BaseModel):
+    data: date = Field(..., description="Data da despesa")
+    descricao: str = Field(..., description="Descrição da despesa")
+    valor: float = Field(..., gt=0, description="Valor da despesa")
+
+class DespesaCreate(DespesaBase):
+    pass
+
+class DespesaUpdate(BaseModel):
+    data: Optional[date]
+    descricao: Optional[str]
+    valor: Optional[float]
+
+class DespesaRead(DespesaBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
