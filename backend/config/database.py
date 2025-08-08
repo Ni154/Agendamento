0721a35086f982ec08149173f1f259f066f5e9f7
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:28:11 -0300

    Update database.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,1 +1,14 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Variáveis SUPABASE_URL e SUPABASE_KEY precisam estar definidas no .env")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
