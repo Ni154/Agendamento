3253da3703009f006c4b2d605e8f8381c05f9598
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:50:01 -0300

    Update usuario.py

--- backend/odels/usuario.py
+++ backend/odels/usuario.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class UsuarioBase(BaseModel):
+    usuario: str = Field(..., min_length=3, max_length=50)
+
+class UsuarioCreate(UsuarioBase):
+    senha: str = Field(..., min_length=6)
+
+class UsuarioUpdate(BaseModel):
+    usuario: Optional[str] = None
+    senha: Optional[str] = None
+
+class UsuarioDB(UsuarioBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
