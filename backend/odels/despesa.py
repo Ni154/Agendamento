19bbe786aa14eb4ec06fe2d6df4ae1a93ae2281b
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:49:24 -0300

    Update despesa.py

--- backend/odels/despesa.py
+++ backend/odels/despesa.py
@@ -1,1 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class DespesaBase(BaseModel):
+    data: date
+    descricao: str
+    valor: float = Field(..., gt=0)
+
+class DespesaCreate(DespesaBase):
+    pass
+
+class DespesaUpdate(BaseModel):
+    data: Optional[date]
+    descricao: Optional[str]
+    valor: Optional[float]
+
+class DespesaDB(DespesaBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
