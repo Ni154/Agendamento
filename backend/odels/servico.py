aa376b25b82a02119ca5c7736dcc03a6f7bc2c3e
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:30:02 -0300

    Update servico.py

--- backend/odels/servico.py
+++ backend/odels/servico.py
@@ -1,1 +1,21 @@
 
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class ServicoBase(BaseModel):
+    nome: str = Field(..., example="Depilação facial")
+    unidade: str = Field(..., example="sessão")
+    quantidade: int = Field(..., ge=0, example=5)
+    valor: float = Field(..., ge=0, example=120.00)
+
+class ServicoCreate(ServicoBase):
+    pass
+
+class ServicoUpdate(ServicoBase):
+    pass
+
+class ServicoDB(ServicoBase):
+    id: int
+
+    class Config:
+        orm_mode = True
