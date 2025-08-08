c33f17d5930b334834a94c637b0f26cec59e4c4b
Ni154 <niedson.santosperola@gmail.com>
2025-08-08 18:29:39 -0300

    Update produto.py

--- backend/odels/produto.py
+++ backend/odels/produto.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class ProdutoBase(BaseModel):
+    nome: str = Field(..., example="Cera depilatória")
+    quantidade: int = Field(..., ge=0, example=10)
+    preco_venda: float = Field(..., ge=0, example=59.90)
+
+class ProdutoCreate(ProdutoBase):
+    pass
+
+class ProdutoUpdate(ProdutoBase):
+    pass
+
+class ProdutoDB(ProdutoBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
