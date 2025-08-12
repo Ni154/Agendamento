b0c31cb8ed19e54d1f10684772ba681d326d75fa
Ni154 <niedson.santosperola@gmail.com>
2025-08-12 12:37:45 -0300

    Update database.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -2,25 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Variáveis do Railway - você pode trocar para ler do .env se preferir
-DB_USER = "postgres"
-DB_PASSWORD = "IItWDGjcnNIDuCQlpGnKeiPhwmoaFhNh"
-DB_HOST = "postgres.railway.internal"
-DB_PORT = "5432"
-DB_NAME = "railway"
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres.railway.internal")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "IItWDGjcnNIDuCQlpGnKeiPhwmoaFhNh")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "railway")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# Dependência para injetar session no FastAPI
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
