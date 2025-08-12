2c3065238eb6bc8618a9d6f33b8d69d01b8078d2
Ni154 <niedson.santosperola@gmail.com>
2025-08-12 12:27:48 -0300

    Update servico.py

--- backend/odels/servico.py
+++ backend/odels/servico.py
@@ -1,21 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from ..config.database import Base
 
-from pydantic import BaseModel, Field
-from typing import Optional
+class Servico(Base):
+    __tablename__ = "servicos"
 
-class ServicoBase(BaseModel):
-    nome: str = Field(..., example="Depilação facial")
-    unidade: str = Field(..., example="sessão")
-    quantidade: int = Field(..., ge=0, example=5)
-    valor: float = Field(..., ge=0, example=120.00)
-
-class ServicoCreate(ServicoBase):
-    pass
-
-class ServicoUpdate(ServicoBase):
-    pass
-
-class ServicoDB(ServicoBase):
-    id: int
-
-    class Config:
-        orm_mode = True
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    unidade = Column(String, nullable=True)
+    quantidade = Column(Integer, default=0)
+    valor = Column(Float, nullable=False)
