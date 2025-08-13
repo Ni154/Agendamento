e7037139d14c4a02d4f66786ef145cc3503357f4
Ni154 <niedson.santosperola@gmail.com>
2025-08-13 12:15:44 -0300

    Create __init__.py

---
+++ backend/odels/__init__.py
@@ -0,0 +1,8 @@
+# Ajuste os nomes se os seus arquivos tiverem outro nome
+from backend.models.cliente import Cliente
+from backend.models.produto import Produto
+from backend.models.servico import Servico
+from backend.models.agendamento import Agendamento
+from backend.models.venda import Venda
+from backend.models.despesa import Despesa
+from backend.models.usuario import Usuario
