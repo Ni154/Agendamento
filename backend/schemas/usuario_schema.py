40b792253a42d71a51dbd7c650305b094f1fb393
Ni154 <niedson.santosperola@gmail.com>
2025-08-13 12:18:29 -0300

    Update usuario_schema.py

--- backend/schemas/usuario_schema.py
+++ backend/schemas/usuario_schema.py
@@ -1,18 +1,9 @@
 from pydantic import BaseModel
 
-class UsuarioCreate(BaseModel):
-    usuario: str
-    senha: str
-    role: str = "user"
-
 class UsuarioLogin(BaseModel):
     usuario: str
     senha: str
 
-class UsuarioResponse(BaseModel):
-    id: int
-    usuario: str
-    role: str
-
-    class Config:
-        orm_mode = True
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
