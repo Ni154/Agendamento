5b28dc67c7926a8c4d5bcc0fb2b8b2c31a411f1e
Ni154 <niedson.santosperola@gmail.com>
2025-08-19 09:29:06 -0300

    Delete backend/odels directory

--- backend/odels/usuario.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.config.database import Base
-
-class Usuario(Base):
-    __tablename__ = "usuarios"
-    id = Column(Integer, primary_key=True, index=True)
-    usuario = Column(String, unique=True, index=True, nullable=False)
-    senha = Column(String, nullable=False)
-    role = Column(String, default="user")  # Pode ser admin, user, etc
