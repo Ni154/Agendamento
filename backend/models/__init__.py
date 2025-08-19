80c6b4fc4174d124b6383e28efbbba3e93abc32c
Ni154 <niedson.santosperola@gmail.com>
2025-08-19 10:02:24 -0300

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,8 @@
 
++from .cliente import Cliente
++from .produto import Produto
++from .servico import Servico
++from .agendamento import Agendamento
++from .venda import Venda
++from .despesa import Despesa
++from .usuario import Usuario
