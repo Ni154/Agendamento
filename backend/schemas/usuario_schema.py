27594a31ce2efd38daa575a6f073095271c45178
Ni154 <niedson.santosperola@gmail.com>
2025-08-23 16:23:29 -0300

    Delete backend/schemas directory

--- backend/schemas/usuario_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UsuarioLogin(BaseModel):
-    usuario: str
-    senha: str
-
-class TokenResponse(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
