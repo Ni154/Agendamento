6a187b44de27d4a9c3037c59f89f3adba9b9bbbd
Ni154 <niedson.santosperola@gmail.com>
2025-09-01 18:49:36 -0300

    Update servico.py

--- backend/schemas/servico.py
+++ backend/schemas/servico.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from decimal import Decimal
+
+class ServicoIn(BaseModel):
+    nome: str
+    valor: Decimal = 0
+
+class ServicoOut(BaseModel):
+    id: str
+    nome: str
+    valor: Decimal
+    class Config:
+        from_attributes = True
 
