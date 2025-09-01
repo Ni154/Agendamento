549d5bab5d9c19dfe17d875a7458f70eee7aafe2
Ni154 <niedson.santosperola@gmail.com>
2025-09-01 15:00:24 -0300

    Delete backend/models directory

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-+from .cliente import Cliente
-+from .produto import Produto
-+from .servico import Servico
-+from .agendamento import Agendamento
-+from .venda import Venda
-+from .despesa import Despesa
-+from .usuario import Usuario
