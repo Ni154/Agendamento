9b62d603005dc0c233b4f269f74871802ced4c53
Ni154 <niedson.santosperola@gmail.com>
2025-09-01 18:49:19 -0300

    Update produto.py

--- backend/schemas/produto.py
+++ backend/schemas/produto.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+from decimal import Decimal
+
+class ProdutoIn(BaseModel):
+    nome: str
+    sku: str | None = None
+    preco: Decimal = 0
+
+class ProdutoOut(BaseModel):
+    id: str
+    nome: str
+    sku: str | None = None
+    preco: Decimal
+    class Config:
+        from_attributes = True
 
