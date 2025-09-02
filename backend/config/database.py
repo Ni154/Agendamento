6396b3991cd1c16f2fc7fee759be62cebd291da5
Ni154 <niedson.santosperola@gmail.com>
2025-09-02 16:10:05 -0300

    Update database.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from settings import settings
+from config.settings import settings
+
 
 connect_args = {}
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20, connect_args=connect_args)
